2c832262c3f276753ac283a8de39d48f6bddd8ba
ericsryan <eric@ericsryan.com>
2021-04-15 01:25:35 -0700

    Added Postgres db settings

--- mineral_catalog/mineral_catalog/deploy_settings/__init__.py
+++ mineral_catalog/mineral_catalog/deploy_settings/__init__.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from mineral_catalog.settings import *
 
 DEBUG = False
@@ -6,3 +8,6 @@ TEMPLATE_DEBUG = DEBUG
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
