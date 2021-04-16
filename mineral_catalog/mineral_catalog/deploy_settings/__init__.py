d73efd1886adf3aa2effa3f6a7374bcd7ec4da9d
ericsryan <eric@ericsryan.com>
2021-04-16 00:02:05 -0700

    Set DEBUG to True

--- mineral_catalog/mineral_catalog/deploy_settings/__init__.py
+++ mineral_catalog/mineral_catalog/deploy_settings/__init__.py
@@ -2,7 +2,7 @@ import dj_database_url
 
 from mineral_catalog.settings import *
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
@@ -11,5 +11,3 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
-#STATICFILES_STORAGE = "whitenoise.django.CompressedManifestStaticFilesStorage"
